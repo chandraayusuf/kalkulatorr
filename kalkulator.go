4ed403f62ae1e83ea0a085e12b19a9285cfd2e60
chandraayusuf <58335208+chandraayusuf@users.noreply.github.com>
2019-12-01 22:41:24 +0800

    kalkulator.go
    pembuatan pertama kali

---
+++ kalkulator.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func repeatHelper() {
+	fmt.Println("Tekan huruf Y jika anda ingin memulai Perhitungan")
+	fmt.Println("Tekan huruf T jika Anda ingin keluar dari apliaksi")
+	fmt.Println("Apakah Anda ingin melakukan perhitungan??")
+}
+func outputHelper() {
+	fmt.Println("Perhitungan dengan format num1 spasi oparasi spasi num2")
+	fmt.Println("Operasi Tersedia = +,-,*,/,^,%")
+	fmt.Println("Ket : ^ operasi pangkat sedangakan % operasi hasil bagi")
+	fmt.Println("Contoh = 1 + 1")
+}
+
+func main() {
+
+}
